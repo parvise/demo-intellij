ba29b414e3a57830d706e311a467243f3f1594af
parvise <parvise.md20@gmail.com>
2022-05-27 16:15:34 +0530

    first-demo

---
+++ src/main/java/Demo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo extends Object {
+
+    public static void main(String[] args) {
+        System.out.println("heloow orld");
+
+        List<String> list = new ArrayList<>();
+        int i = 10;
+        for (int i1 = 0; i1 <= i; i1++) {
+            System.out.println("Test : " +i1);
+        }
+    }
+
+    public void add(int a) {
+    }
+
+}
